57edd69dcad7b141b437738e0f946059ca74435d
Varde <varde@Varde-Mac-mini.local>
2015-09-27 14:05:54 +0200

    config

--- index.js
+++ index.js
@@ -22,19 +22,19 @@ console.log(buf);*/
 function ArtNetController(host, port) {
   this._host = host;
   this._port = port;
-  this._socket = dgram.createSocket("udp4");
+  this._socket = dgram.createSocket({type:"udp4", reuseAddr:true, });
 
   this.nodes = [];
 
 
   var me = this;
   this._socket.on('message', function(msg, rinfo) {
-    console.log('Received %d bytes from %s:%d\n',
-      msg.length, rinfo.address, rinfo.port);
+   // console.log('Received %d bytes from %s:%d\n',
+      //msg.length, rinfo.address, rinfo.port);
 
       ArtnetPacket.parse(msg).then(function(m){
         if(m.code == 'ArtPollReply'){
-          //console.log(m);
+       //   console.log(m);
 
           var node = _.find(me.nodes, function(n){
             return n.ip == m.IP.join('.');
@@ -43,10 +43,18 @@ function ArtNetController(host, port) {
 
           if(!node){
             node = new ArtnetNode();
-            node.ip = m.IP.join('.');
-            node.mac = _.map(m.MAC, function(n){return n.toString(16)}).join(':');
+            //console.log(m)
             me.nodes.push(node);
-          }
+          } 
+          
+          node.ip = m.IP.join('.');
+          node.mac = _.map(m.MAC, function(n){return n.toString(16)}).join(':');
+          node.name = m.LongName;
+          node.version = m.VersionInfoH+"."+m.VersionInfoL;
+          node.numOutputs = m.NumPortsLo;
+          node.universesOutput = m.SwOut;
+          node.subnet = m.SubSwitch;
+          node.net = m.NetSwitch;
 
           node._waitingForPollReply = false;
         }
@@ -55,7 +63,7 @@ function ArtNetController(host, port) {
 
   this._socket.on('listening', function () {
     var address = me._socket.address();
-    console.log('UDP Client listening on ' + address.address + ":" + address.port);
+  //  console.log('UDP Client listening on ' + address.address + ":" + address.port);
   });
 
   this._socket.bind({port:port}, function(){
@@ -70,7 +78,7 @@ function ArtNetController(host, port) {
 
 
 ArtNetController.prototype.refreshClients = function(){
-  console.log("Frefresh.... Nodes: "+this.nodes.length);
+  //console.log("Frefresh.... Nodes: "+this.nodes.length);
 
   this.nodes = _.filter(this.nodes, function(node){
     return node._waitingForPollReply == false;
@@ -82,11 +90,41 @@ ArtNetController.prototype.refreshClients = function(){
 
 
   var buf = ArtnetPacket.createArtPoll();
-  this._socket.send(buf,0, buf.length, this._port, "10.255.255.255")
+  this._socket.send(buf,0, buf.length, this._port, "2.255.255.255")
 
   setTimeout(function(_this){
     _this.refreshClients()
   },2500, this);
+}
+
+ArtNetController.prototype.updateClient = function(ip, name, universes, locate){
+
+  if(universes){
+    var portOutput = [];
+    for(var i=0;i<4;i++){
+      portOutput[i] = universes[i] & 0xF
+    }
+
+    var subSwitch = (universes[0] & 0xF0) >> 4;
+    var netSwitch = (universes[0] & 0xF00) >> 8;
+  } else {
+    var portOutput = undefined;
+    var netSwitch = undefined;
+    var subSwitch = undefined;
+  }
+
+  var buf = ArtnetPacket.createProgramAddress({
+    shortName: name, 
+    longName: name,
+    portOutput: portOutput,
+    subSwitch: subSwitch,
+    netSwitch: netSwitch,
+    command: locate?0x04:0x00
+  });
+  /*console.log({portOutput: portOutput,
+    subSwitch: subSwitch,
+    netSwitch: netSwitch})*/
+  this._socket.send(buf,0, buf.length, this._port, ip);
 }
 
 
